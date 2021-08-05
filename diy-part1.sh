a21ff9c31039ee36440e38cd5bba3bf7c7307614
lolligoo <22412133+lolligoo@users.noreply.github.com>
2021-08-05 23:49:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git jerrykuku https://github.com/jerrykuku/openwrt-package.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
